ff51e6f9b3abc2ee589af8b426df80d9f02a140f
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2026-03-06 19:04:45 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="h-screen w-full flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-bold text-center tracking-tight">OpenCare Project</h1>
+    </div>
+  )
+}
+
+export default App
