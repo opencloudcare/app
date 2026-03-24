757fc37ce41cf81e8819e1fcca1b0eca259599af
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2026-03-24 10:15:15 -0700

    setup shadcn/ui and path aliases

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router'
-import { authClient } from '../utils/auth'
+import { authClient } from '@/lib/auth.ts'
 
 interface DashboardData {
     message: string
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from 'react'
 import {useNavigate} from 'react-router'
-import {authClient} from '../utils/auth'
+import {authClient} from '@/lib/auth.ts'
 
 export function SignIn() {
     const [email, setEmail] = useState('')
